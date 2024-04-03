5f04f43fa9f1aa1af5dc28bf58bb903f2029fc57
Arseniy132 <159192121+Arseniy132@users.noreply.github.com>
2024-04-03 16:20:52 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import streamlit as st
 
 
@@ -8,3 +9,13 @@ st.subheader("Привіт :wave:, це початковий веб")
 st.title("Я пробую зробити мій власний веб сайт")
 st.write("Мені подобаєця знавати нові речі наприклад як працює самальот і програмуваня з пайтон")
 st.write("[Як я цей сайт зробив >>](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+
+an225 = Image.open("images/an225.png")
+Py = Image.open("images/Py.png")
+
+with st.container():
+    image_column, image_column2 = st.columns((1, 2))
+    with image_column:
+        st.image(Py)
+    with image_column2:
+        st.image(an225)
