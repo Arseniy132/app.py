81b0b4d54a41f15450f0acd9fd201003de50704a
Arseniy132 <159192121+Arseniy132@users.noreply.github.com>
2024-04-03 15:40:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ import streamlit as st
 st.set_page_config(page_title="My website", page_icon=":upside_down_face:", layout="wide")
 
 
-st.subheader("Привіт :wave:, це тест")
+st.subheader("Привіт :wave:, це початковий веб")
+st.title("Я пробую зробити мій власний веб сайт")
+st.write("Мені подобаєця знавати нові речі наприклад як працює самальот і програмуваня з пайтон")
+st.write("[Як я цей сайт зробив >>](https://www.youtube.com/watch?v=VqgUkExPvLY)")
