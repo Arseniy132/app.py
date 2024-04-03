0699020cea42e2c29beaa68035da50b6bf8652f0
Arseniy132 <159192121+Arseniy132@users.noreply.github.com>
2024-04-03 20:48:01 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -22,12 +22,39 @@ with st.container():
     with image_column2:
         st.image(an225)
 
-contact_form = """
-<form action="https://formsubmit.co/arseniykao@gmail.com" method="POST">
-     <input type="text" name="name" required>
-     <input type="email" name="email" required>
-     <button type="submit">Send</button>
-</form>
-"""
+with st.container():
+    text_column, download_column = st.columns((1, 2))
+    with text_column:
+        text_contents = '''
+from PIL import Image
+import streamlit as st
 
-st.markdown(contact_form, unsafe_allow_html=True)
+st.set_page_config(page_title="My website", page_icon=":upside_down_face:", layout="wide")
+
+st.subheader("Привіт :wave:, це початковий веб")
+st.title("Я пробую зробити мій власний веб сайт")
+st.write("Мені подобаєця знавати нові речі наприклад як працює самальот і програмуваня з пайтон")
+st.write("[Як я цей сайт зробив >>](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+
+
+#-----------assets---------------
+an225 = Image.open("images/an225.png")
+Py = Image.open("images/Py.png")
+
+
+#----------containers------------
+with st.container():
+    image_column, image_column2 = st.columns((1, 2))
+    with image_column:
+        st.image(Py)
+    with image_column2:
+        st.image(an225)
+
+with st.container():
+    text_column, download_column = st.columns((1, 2))
+    with text_column:
+        text_contents = ''''''
+    with download_column:
+        st.download_button('app.py', text_contents)'''
+    with download_column:
+        st.download_button('app.py', text_contents)
