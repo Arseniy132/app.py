be2f42cfc13e9b65f50ad5136125d721752b35a7
Arseniy132 <159192121+Arseniy132@users.noreply.github.com>
2024-04-09 15:57:54 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Бульба", page_icon=":face_with_monocle:")
 with st.sidebar:
     selected = option_menu(
         menu_title="Main Menu",
-        options=["Home", "Projects"],
+        options=["Home", "Projects", "Spanish", "English"],
     )
 
 
@@ -37,6 +37,61 @@ if selected == "Home":
     if st.button("Музика", type="primary"):
         st.audio("audio/Four-Seasons-Vivaldi.ogg")
 
+
+if selected == "Projects":
+    st.title("[thistest312.streamlit.app](https://github.com/Arseniy132/app.py)")
+
+if selected == "Spanish":
+    st.subheader("Hola :wave:, esto es un sitio web principal")
+    st.title("Estoy intentando crear mi propio sitio web")
+    st.write("Me gusta saber nuevas cosas, por ejemplo, como funciona un avion y me gusta la programacion con Python")
+    st.write("[Como he hecho este sitio web >>](https://www.youtube.com/playlist?list=PL7QI8ORyVSCaejt2LICRQtOTwmPiwKO2n)")
+
+    # -----------assets---------------
+    an225 = Image.open("images/an225.png")
+    Py = Image.open("images/Py.png")
+
+    # ----------containers------------
+    with st.container():
+        image_column, image_column2 = st.columns((1, 2))
+        with image_column:
+            st.image(Py)
+        with image_column2:
+            st.image(an225)
+
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    if st.button("Musica", type="primary"):
+        st.audio("audio/Four-Seasons-Vivaldi.ogg")
+
+if selected == "English":
+    st.subheader("Hello :wave:, this is a starter web")
+    st.title("I'm trying to create my own web site")
+    st.write("I like discovering")
+    st.write("[Як я цей сайт зробив >>](https://www.youtube.com/playlist?list=PL7QI8ORyVSCaejt2LICRQtOTwmPiwKO2n)")
+
+    # -----------assets---------------
+    an225 = Image.open("images/an225.png")
+    Py = Image.open("images/Py.png")
+
+    # ----------containers------------
+    with st.container():
+        image_column, image_column2 = st.columns((1, 2))
+        with image_column:
+            st.image(Py)
+        with image_column2:
+            st.image(an225)
+
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    if st.button("Музика", type="primary"):
+        st.audio("audio/Four-Seasons-Vivaldi.ogg")
 
 if selected == "Projects":
     st.title("[thistest312.streamlit.app](https://github.com/Arseniy132/app.py)")
