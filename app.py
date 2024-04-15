d79c3493acfbe296eb649f2b69dc5820cf98e80e
Arseniy132 <159192121+Arseniy132@users.noreply.github.com>
2024-04-15 21:36:02 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import openai
 from PIL import Image
 import streamlit as st
 from streamlit_option_menu import option_menu
+
 
 im = Image.open("Python.png")
 
@@ -19,7 +21,7 @@ st.set_page_config(page_title="Бульба", page_icon=im)
 with st.sidebar:
     selected = option_menu(
         menu_title="Selection Menu",
-        options=["Українська", "Español", "English", "Projects"],
+        options=["Українська", "Español", "English", "Projects", "Bot"],
     )
 
 
@@ -143,3 +145,43 @@ if selected == "English":
 
 if selected == "Projects":
     st.title("[thistest312.streamlit.app](https://github.com/Arseniy132/app.py)")
+
+
+
+
+
+if selected == "Bot":
+    openai.api_key = st.secrets["KEY"]
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    prompt = st.chat_input("What's up?")
+    if prompt:
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + " |")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
