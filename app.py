aae4cfa4e89cfee894e4a0cd22204cd6a6250e05
Arseniy132 <159192121+Arseniy132@users.noreply.github.com>
2024-04-15 21:55:20 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from openai import OpenAI
 from PIL import Image
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -20,7 +19,7 @@ st.set_page_config(page_title="Бульба", page_icon=im)
 with st.sidebar:
     selected = option_menu(
         menu_title="Selection Menu",
-        options=["Українська", "Español", "English", "Projects", "Bot"],
+        options=["Українська", "Español", "English", "Projects"],
     )
 
 
@@ -144,37 +143,3 @@ if selected == "English":
 
 if selected == "Projects":
     st.title("[thistest312.streamlit.app](https://github.com/Arseniy132/app.py)")
-
-
-
-
-
-if selected == "Bot":
-    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if prompt := st.chat_input("What is up?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
